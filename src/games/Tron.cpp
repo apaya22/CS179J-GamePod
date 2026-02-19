d987eb4773ea6ec29432c1b19a952066968e7bd9
ZWang0987 <zwang577@ucr.edu>
2026-02-19 12:43:01 -0800

    confirmed character sprite with teammate will need to make new sprites and other collosision, these up coming updates are left as comments in code

--- src/games/Tron.cpp
+++ src/games/Tron.cpp
@@ -87,7 +87,25 @@ const uint16_t characterSpriteRight[9] = {
   0b001000000
 };
 
-//will take an x and y location and direction from the server and then based off that draw a character on the screen
+  //for future new 9x5 bike spirte with trail guards(going right), trail needs to come off middle of the bike too and somehow need to figure our overlap logic 
+  // of bike wheel on turns
+  //would also be nice to figure out how to color the bike tail just white for all bikes along with wheels maybe
+  //need to figure out bike overlap on turns with trail
+  //need to track trail locally for deletions
+  //need to make death animation
+
+  // 0b000000000,
+  // 0b000000000,
+  // 0b000110100,
+  // 0b111111110,
+  // 0b000111111,
+  // 0b111111110,
+  // 0b000110100,
+  // 0b000000000,
+  // 0b000000000
+
+//will take an x and y location and direction from the server and then based off that draw a character 9x9 on the screen
+//this will also need to intake a color for the draw pixels
 void drawCharacter(int character_x_cord, int character_y_cord, uint8_t character_direction){
     
   //here we need to draw from the top left X and Y cords meaning the center point if the fifth
@@ -96,7 +114,7 @@ void drawCharacter(int character_x_cord, int character_y_cord, uint8_t character
   int character_top_left_y = character_y_cord - 4;
 
   //pinter for character sprite direction, may make it a global
-  const uint16_t* sprite = characterSpriteUp;
+  const uint16_t* sprite = 0;
 
   //Slect character sprite based on current direction 0-3 up,down,left,right
   if (character_direction == 0){
@@ -149,9 +167,35 @@ void clearCharacter(int character_x_cord, int character_y_cord){
 
       for (int col = 0; col < 9; col++) {
 
-              tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_BLACK);
+        tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_BLACK);
       }
   }
+}
+
+//will just leave behind a 1x1 trail based on the players directions (will also need a character color)
+void drawTrail(int character_x_cord, int character_y_cord, uint8_t character_direction){
+
+  int trailX, trailY = 0;
+
+  //if the character is facing up
+  if(character_direction == 0){
+    trailX = character_x_cord;
+    trailY = character_y_cord - 5;
+    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
+  }else if(character_direction ==1){
+    trailX = character_x_cord;
+    trailY = character_y_cord + 5;
+    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
+  }else if(character_direction ==2){
+    trailX = character_x_cord + 5;
+    trailY = character_y_cord;
+    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
+  }else{
+    trailX = character_x_cord - 5;
+    trailY = character_y_cord;
+    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
+  }
+
 }
 
 void setup() {
@@ -170,28 +214,53 @@ void setup() {
   resetScreenBeforeStart();
 }
 
+
+  int dummyx = 100;
+  int dummyy= 100;
+  int dummyd= 3;
 void loop() {
 
-  int dummyx = 125;
-  int dummyy= 125;
-  int dummyd= 0;
+  //testing for onscreen and how it looks
+  // int dummyx = 125;
+  // int dummyy= 125;
+  // int dummyd= 0;
+  // drawCharacter(dummyx,dummyy,dummyd);
+
+  // dummyx = 50;
+  // dummyy= 50;
+  // dummyd= 1;
+  // drawCharacter(dummyx,dummyy,dummyd);
+
+  // dummyx = 200;
+  // dummyy= 200;
+  // dummyd= 2;
+  // drawCharacter(dummyx,dummyy,dummyd);
+
+  // dummyx = 300;
+  // dummyy= 200;
+  // dummyd= 3;
+  // drawCharacter(dummyx,dummyy,dummyd);
+
+  // // clearCharacter(dummyx,dummyy);
+
+  // dummyx = 177;
+  // dummyy= 177;
+  // dummyd= 3;
+  // drawTrail(dummyx,dummyy,dummyd);
+
+
+
+
+  delay(50);
+  drawCharacter(dummyx,dummyy,dummyd);
+  drawTrail(dummyx,dummyy,dummyd);
+  clearCharacter(dummyx,dummyy);
+  dummyx++;
   drawCharacter(dummyx,dummyy,dummyd);
 
-  dummyx = 50;
-  dummyy= 50;
-  dummyd= 1;
-  drawCharacter(dummyx,dummyy,dummyd);
-
-  dummyx = 200;
-  dummyy= 200;
-  dummyd= 2;
-  drawCharacter(dummyx,dummyy,dummyd);
-
-  dummyx = 300;
-  dummyy= 200;
-  dummyd= 3;
-  drawCharacter(dummyx,dummyy,dummyd);
-
-  // clearCharacter(dummyx,dummyy);
+  // dummyx = 177;
+  // dummyy= 177;
+  // dummyd= 3;
+  // drawTrail(dummyx,dummyy,dummyd);
 
 }
