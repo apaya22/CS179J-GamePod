98c0f5e91bc3ed5b016cac19e5badaddb808c005
ZWang0987 <zwang577@ucr.edu>
2026-02-19 16:41:03 -0800

    currently bike is a 9x5 or 5x9 depeding on direction and there is a trail guard that will hold the trail until a turn   the trail is working and bike is working   have yet to figure out turning and bike over lapping with a trail and how i will recolor the trail after the bike is not on it anymore   need to figure out how to color certain pixels on the bike difference colors   need to figure out how to keep track of which pixles are apart of the trail in order for deletion later, this will also be used for over lap checks   need to make death animation

--- src/games/Tron.cpp
+++ src/games/Tron.cpp
@@ -9,6 +9,13 @@ static const int Width_Sreen = 320;
 static const int Height_screen = 240;
 static const uint8_t Screen_ROT = 1; //1 is for horzontal depending on 0/1/2/3 u will need to switch the w and h
 
+  //currently bike is a 9x5 or 5x9 depeding on direction and there is a trail guard that will hold the trail until a turn
+  //the trail is working and bike is working
+  //have yet to figure out turning and bike over lapping with a trail and how i will recolor the trail after the bike is not on it anymore
+  //need to figure out how to color certain pixels on the bike difference colors
+  //need to figure out how to keep track of which pixles are apart of the trail in order for deletion later, this will also be used for over lap checks
+  //need to make death animation
+
 // =====================
 // DISPLAY PIN CONFIGURATION (your new wiring)
 // =====================
@@ -35,83 +42,57 @@ void resetScreenBeforeStart() {
   tft.fillScreen(ILI9341_BLACK);
 }
 
-//will hold value for a 9x9 character sprite(UP)
+//will hold value for a 9x5 character sprite(UP)
 const uint16_t characterSpriteUp[9] = {
-  0b000010000,
-  0b000111000,
-  0b001111100,
-  0b001111100,
-  0b000111000,
-  0b000111000,
-  0b111111111,
-  0b001111100,
-  0b000111000
+  0b00100,
+  0b01110,
+  0b11111,
+  0b01110,
+  0b11111,
+  0b11011,
+  0b01010,
+  0b01010,
+  0b01010
 };
 
-//will hold value for a 9x9 character sprite(Down)
+//will hold value for a 9x5 character sprite(Down)
 const uint16_t characterSpriteDown[9] = {
-  0b000111000,
-  0b001111100,
-  0b111111111,
-  0b000111000,
-  0b000111000,
-  0b001111100,
-  0b001111100,
-  0b000111000,
-  0b000010000
+  0b01010,
+  0b01010,
+  0b01010,
+  0b11011,
+  0b11111,
+  0b01110,
+  0b11111,
+  0b01110,
+  0b00100
 };
 
-//will hold value for a 9x9 character sprite(Left)
-const uint16_t characterSpriteLeft[9] = {
-  0b000000100,
-  0b000000100,
-  0b001100110,
+//will hold value for a 9x5 character sprite(Left)
+const uint16_t characterSpriteLeft[5] = {
+  0b001011000,
   0b011111111,
-  0b111111111,
+  0b111110000,
   0b011111111,
-  0b001100110,
-  0b000000100,
-  0b000000100
+  0b001011000
 };
 
-//will hold value for a 9x9 character sprite(Right)
-const uint16_t characterSpriteRight[9] = {
-  0b001000000,
-  0b001000000,
-  0b011001100,
+//will hold value for a 9x5 character sprite(Right)
+const uint16_t characterSpriteRight[5] = {
+  0b000110100,
   0b111111110,
-  0b111111111,
+  0b000011111,
   0b111111110,
-  0b011001100,
-  0b001000000,
-  0b001000000
+  0b000110100
 };
-
-  //for future new 9x5 bike spirte with trail guards(going right), trail needs to come off middle of the bike too and somehow need to figure our overlap logic 
-  // of bike wheel on turns
-  //would also be nice to figure out how to color the bike tail just white for all bikes along with wheels maybe
-  //need to figure out bike overlap on turns with trail
-  //need to track trail locally for deletions
-  //need to make death animation
-
-  // 0b000000000,
-  // 0b000000000,
-  // 0b000110100,
-  // 0b111111110,
-  // 0b000111111,
-  // 0b111111110,
-  // 0b000110100,
-  // 0b000000000,
-  // 0b000000000
 
 //will take an x and y location and direction from the server and then based off that draw a character 9x9 on the screen
 //this will also need to intake a color for the draw pixels
 void drawCharacter(int character_x_cord, int character_y_cord, uint8_t character_direction){
     
-  //here we need to draw from the top left X and Y cords meaning the center point if the fifth
-  //bit of our 9x9 sprite 0bxxxx1xxxx
-  int character_top_left_x = character_x_cord - 4;
-  int character_top_left_y = character_y_cord - 4;
+  //we need to draw form the top left x,y cordinate of the sprite
+  int character_top_left_x = 0;
+  int character_top_left_y = 0;
 
   //pinter for character sprite direction, may make it a global
   const uint16_t* sprite = 0;
@@ -121,82 +102,227 @@ void drawCharacter(int character_x_cord, int character_y_cord, uint8_t character
 
     //up
     sprite = characterSpriteUp;
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxx1xx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    character_top_left_x = character_x_cord - 2;
+    character_top_left_y = character_y_cord - 4;
 
   } else if (character_direction == 1){
 
     //down
     sprite = characterSpriteDown;
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxx1xx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    // 0bxxxxx
+    character_top_left_x = character_x_cord - 2;
+    character_top_left_y = character_y_cord - 4;
 
   } else if (character_direction == 2){
 
     //left
     sprite = characterSpriteLeft;
+    // 0bxxxxxxxxx,
+    // 0bxxxxxxxxx,
+    // 0bxxxx1xxxx,
+    // 0bxxxxxxxxx,
+    // 0bxxxxxxxxx,
+    character_top_left_x = character_x_cord - 4;
+    character_top_left_y = character_y_cord - 2;
 
   } else {
 
     //right
     sprite = characterSpriteRight;
+    // 0bxxxxxxxxx,
+    // 0bxxxxxxxxx,
+    // 0bxxxx1xxxx,
+    // 0bxxxxxxxxx,
+    // 0bxxxxxxxxx,
+    character_top_left_x = character_x_cord - 4;
+    character_top_left_y = character_y_cord - 2;
 
   }
 
-  //logic for drawing character(made it into a for loop, can do this statically but I dont think its worth)
-  //essentailly this goes through all 9 rows and collumns of the sprite and colors the pixles on the screen
-  for (int row = 0; row < 9; row++) {
-      uint16_t row_data = sprite[row];
+  if (character_direction == 0 || character_direction == 1){
+    //if the character if facing up or down it is a 9x5 so here we go through a 9x5 and draws each pixle
+    //needs to find a way to draw certain pixles a certain color 
+    for (int row = 0; row < 9; row++) {
+        uint16_t row_data = sprite[row];
 
-      for (int col = 0; col < 9; col++) {
+        for (int col = 0; col < 5; col++) {
 
-          //checks to see if the bit in the row x column is 1 of not
-          bool draw_pixel = (row_data >> (8 - col)) & 1;
+            //checks to see if the bit in the row x column is 1 of not here since 5 bits we do highest bit index 4
+            bool draw_pixel = (row_data >> (4 - col)) & 1;
 
-          if (draw_pixel) {
-              tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_CYAN);
-          }
-      }
+            if (draw_pixel) {
+                tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_CYAN);
+            }
+        }
+    }
+  }else{
+    //if the character if facing up or down it is a 5x9 so here we go through a 5x9 and draws each pixle
+    //needs to find a way to draw certain pixles a certain color 
+      for (int row = 0; row < 5; row++) {
+        uint16_t row_data = sprite[row];
+
+        for (int col = 0; col < 9; col++) {
+
+            //checks to see if the bit in the row x column is 1 of not here we do highest bit index 8
+            bool draw_pixel = (row_data >> (8 - col)) & 1;
+
+            if (draw_pixel) {
+                tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_CYAN);
+            }
+        }
+    }
   }
-
 }
 
-//this should clear a 9x9 grid of the character
-void clearCharacter(int character_x_cord, int character_y_cord){
 
-  int character_top_left_x = character_x_cord - 4;
-  int character_top_left_y = character_y_cord - 4;
+//will hold value for a 9x5 character sprite(UP) clearing and leaving the pixles for the
+//trail guards and trail if 1 clear bit else leave it
+const uint16_t clearCharacterSpriteUp[9] = {
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11011,
+  0b11011,
+  0b11011,
+  0b11011,
+  0b11011
+};
 
+//will hold value for a 9x5 character sprite(DOWN) clearing and leaving the pixles for the
+//trail guards and trail if 1 clear bit else leave it
+const uint16_t clearCharacterSpriteDown[9] = {
+  0b11011,
+  0b11011,
+  0b11011,
+  0b11011,
+  0b11011,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111
+};
+
+//will hold value for a 9x5 character sprite(Left) clearing and leaving the pixles for the
+//trail guards and trail if 1 clear bit else leave it
+const uint16_t clearCharacterSpriteLeft[5] = {
+  0b111111111,
+  0b011111111,
+  0b111100000,
+  0b011111111,
+  0b111111111
+};
+
+//will hold value for a 9x5 character sprite(Right) clearing and leaving the pixles for the
+//trail guards and trail if 1 clear bit else leave it
+const uint16_t clearCharacterSpriteRight[5] = {
+  0b111111111,
+  0b111111111,
+  0b000001111,
+  0b111111111,
+  0b111111111
+};
+
+//this should clear a 9x5 or 5x9 grid of the character
+void clearCharacter(int character_x_cord, int character_y_cord, uint8_t character_direction){
+
+  //we need to draw form the top left x,y cordinate of the sprite
+  int character_top_left_x = 0;
+  int character_top_left_y = 0;
+
+  //pinter for character sprite direction, may make it a global
+  const uint16_t* clearSprite = 0;
+
+  //Slect character sprite based on current direction 0-3 up,down,left,right
+  if (character_direction == 0){
+
+    //up
+    clearSprite = clearCharacterSpriteUp;
+    character_top_left_x = character_x_cord - 2;
+    character_top_left_y = character_y_cord - 4;
+
+  } else if (character_direction == 1){
+
+    //down
+    clearSprite = clearCharacterSpriteDown;
+    character_top_left_x = character_x_cord - 2;
+    character_top_left_y = character_y_cord - 4;
+
+  } else if (character_direction == 2){
+
+    //left
+    clearSprite = clearCharacterSpriteLeft;
+    character_top_left_x = character_x_cord - 4;
+    character_top_left_y = character_y_cord - 2;
+
+  } else {
+
+    //right
+    clearSprite = clearCharacterSpriteRight;
+    character_top_left_x = character_x_cord - 4;
+    character_top_left_y = character_y_cord - 2;
+
+  }
+
+  if (character_direction == 0 || character_direction == 1){
+    //if the character if facing up or down it is a 9x5 so here we go through a 9x5 and draws each pixle
+    //needs to find a way to draw certain pixles a certain color 
     for (int row = 0; row < 9; row++) {
+        uint16_t row_data = clearSprite[row];
 
-      for (int col = 0; col < 9; col++) {
+        for (int col = 0; col < 5; col++) {
 
-        tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_BLACK);
-      }
+            //checks to see if the bit in the row x column is 1 of not here since 5 bits we do highest bit index 4
+            bool draw_pixel = (row_data >> (4 - col)) & 1;
+
+            if (draw_pixel) {
+                tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_BLACK);
+            }
+        }
+    }
+  }else{
+    //if the character if facing up or down it is a 5x9 so here we go through a 5x9 and draws each pixle
+    //needs to find a way to draw certain pixles a certain color 
+      for (int row = 0; row < 5; row++) {
+        uint16_t row_data = clearSprite[row];
+
+        for (int col = 0; col < 9; col++) {
+
+            //checks to see if the bit in the row x column is 1 of not here we do highest bit index 8
+            bool draw_pixel = (row_data >> (8 - col)) & 1;
+
+            if (draw_pixel) {
+                tft.drawPixel(character_top_left_x + col, character_top_left_y + row, ILI9341_BLACK);
+            }
+        }
+    }
   }
 }
 
 //will just leave behind a 1x1 trail based on the players directions (will also need a character color)
-void drawTrail(int character_x_cord, int character_y_cord, uint8_t character_direction){
+// void drawTrail(int character_x_cord, int character_y_cord, uint8_t character_direction){
 
-  int trailX, trailY = 0;
+//   int trailX, trailY = 0;
+//   tft.drawPixel(character_x_cord,character_y_cord, ILI9341_WHITE);
 
-  //if the character is facing up
-  if(character_direction == 0){
-    trailX = character_x_cord;
-    trailY = character_y_cord - 5;
-    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
-  }else if(character_direction ==1){
-    trailX = character_x_cord;
-    trailY = character_y_cord + 5;
-    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
-  }else if(character_direction ==2){
-    trailX = character_x_cord + 5;
-    trailY = character_y_cord;
-    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
-  }else{
-    trailX = character_x_cord - 5;
-    trailY = character_y_cord;
-    tft.drawPixel(trailX,trailY, ILI9341_CYAN);
-  }
-
-}
+// }
 
 void setup() {
   Serial.begin(115200);
@@ -215,8 +341,8 @@ void setup() {
 }
 
 
-  int dummyx = 100;
-  int dummyy= 100;
+  int dummyx = 30;
+  int dummyy= 200;
   int dummyd= 3;
 void loop() {
 
@@ -252,11 +378,12 @@ void loop() {
 
 
   delay(50);
-  drawCharacter(dummyx,dummyy,dummyd);
-  drawTrail(dummyx,dummyy,dummyd);
-  clearCharacter(dummyx,dummyy);
+  //drawCharacter(dummyx,dummyy,dummyd);
+  clearCharacter(dummyx,dummyy,dummyd);
   dummyx++;
+  //dummyy--;
   drawCharacter(dummyx,dummyy,dummyd);
+  //drawTrail(dummyx,dummyy,dummyd);
 
   // dummyx = 177;
   // dummyy= 177;
